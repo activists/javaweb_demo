f7566b41ea5c181475758a3995faca442b7cb8b4
fengfw <451488920@qq.com>
2018-02-01 16:40:23 +0800

    update

--- src/main/java/com/ipinyou/demo/CookiesUtil.java
+++ src/main/java/com/ipinyou/demo/CookiesUtil.java
@@ -1,19 +1,19 @@
-package com.ipinyou.demo;
-
-import javax.servlet.http.*;
-import java.util.HashMap;
-import java.util.Map;
-
-public class CookiesUtil{
-    public Map<String, Cookie> readCookieMap(HttpServletRequest request) {
-        Map<String, Cookie> cookieMap = new HashMap<>();
-        Cookie[] cookies = request.getCookies();
-        if (null != cookies) {
-            for (Cookie cookie : cookies) {
-                cookieMap.put(cookie.getName(), cookie);
-            }
-        }
-        return cookieMap;
-    }
-
-}
+//package com.ipinyou.demo;
+//
+//import javax.servlet.http.*;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class CookiesUtil{
+//    public Map<String, Cookie> readCookieMap(HttpServletRequest request) {
+//        Map<String, Cookie> cookieMap = new HashMap<>();
+//        Cookie[] cookies = request.getCookies();
+//        if (null != cookies) {
+//            for (Cookie cookie : cookies) {
+//                cookieMap.put(cookie.getName(), cookie);
+//            }
+//        }
+//        return cookieMap;
+//    }
+//
+//}
